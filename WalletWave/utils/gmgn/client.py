5a26b6800a126a43c8981d869e54849c2d29154f
astrebig2 <astrebig2@gmail.com>
2025-01-06 22:16:22 -0500

    Added header rotation

--- WalletWave/utils/gmgn/client.py
+++ WalletWave/utils/gmgn/client.py
@@ -2,6 +2,8 @@ import random
 import tls_client
 from fake_useragent import UserAgent
 from typing import Optional, Dict, Any
+import time
+
 from requests.exceptions import HTTPError
 
 from WalletWave.utils.config_validators import validate_wallet_tag
@@ -87,7 +89,6 @@ class AgentMapper:
         user_agent = self.get_user_agent(client_identifier)
         return client_identifier, user_agent
 
-
 class Gmgn:
     BASE_URL = "https://gmgn.ai/defi/quotation"
 
@@ -103,11 +104,14 @@ class Gmgn:
     # TODO: Validate wallet address format in `get_wallet_info` to avoid unnecessary API calls.
     # TODO: Explore rate-limiting compliance for `gmgn.ai` API to avoid potential issues. (2 seconds)
 
-    def __init__(self):
-        self.agent_mapper = AgentMapper
+    def __init__(self, max_requests_range: tuple = (1, 10)):
+        self.agent_mapper = AgentMapper()
         self.session = tls_client.Session(random_tls_extension_order=True)
-        self.client, self.agent = self.agent_mapper.get_random_client_and_agent()
-        self.headers = self._generate_headers()
+        self.client, self.agent, self.headers = None, None, None
+        self.request_count = 0
+        self.max_requests_range = max_requests_range
+        self.max_requests = random.randint(*self.max_requests_range)
+        self._rotate_headers()
 
     def _generate_headers(self) -> Dict[str, str]:
         return {
@@ -120,10 +124,29 @@ class Gmgn:
             "user-agent": self.agent,
         }
 
+    def _rotate_headers(self):
+        self.client, self.agent = self.agent_mapper.get_random_client_and_agent()
+        self.headers = self._generate_headers()
+        print(f"Rotated Headers -> Client: {self.client}, User-Agent: {self.agent}") #todo change to logger
+
     def _make_request(self, endpoint: str, params: Optional[Dict[str, Any]] = None) -> dict:
         url = f"{self.BASE_URL}{endpoint}"
         try:
+            self.request_count += 1
+            if self.request_count % self.max_requests == 0:
+                self._rotate_headers()
+                self.max_requests = random.randint(1, 10)
+                # todo add logger
+
+            time.sleep(2)
             response = self.session.get(url, headers=self.headers, params=params)
+
+            # rotate on rate-limit or block errors
+            if response.status_code in [429, 403]:
+                print(f"Encountered HTTP {response.status_code}, rotating headers and retrying...") #todo change to logger
+                self._rotate_headers()
+                time.sleep(random.randint(5, 10)) # backoff
+                return self._make_request(endpoint, params)
 
             if response.status_code >= 400:
                 raise RuntimeError(f"HTTP error {response.status_code}: {response.text}")
